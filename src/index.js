260489b647f495d1b300e87e7124b8661752a51f
mabrursatori <mabrursatori@gmail.com>
2022-05-20 17:32:48 +0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -152,14 +152,139 @@ function Square(props) {
   // );
 
   //======================================
-  function tick() {
-    const element = (
+  // function tick() {
+  //   const element = (
+  //     <div>
+  //       <h1>Hello, world!</h1>
+  //       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  //     </div>
+  //   );
+  //   ReactDOM.render(element, document.getElementById('root'));
+  // }
+  
+  // setInterval(tick, 1000);
+
+  //===============================================
+  // class Toggle extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {isToggleOn: true};
+  
+  //     // cara binding seperti ini diperlukan untuk membuat `this` dapat berfungsi -
+  //     // pada callback binding
+  //    // this.handleClick = this.handleClick.bind(this);
+  //   }
+  //   handleClick() {
+  //     console.log(this)
+  //     this.setState(state => ({
+  //       isToggleOn: !state.isToggleOn
+  //     }));
+  //   }
+  
+  //   render() {
+  //     return (
+  //       <button onClick={(e) => this.handleClick(e)}>
+  //         {this.state.isToggleOn ? 'ON' : 'OFF'}
+  //       </button>
+  //     );
+  //   }
+  // }
+  
+  // ReactDOM.render(
+  //   <Toggle />,
+  //   document.getElementById('root')
+  // );
+
+  //====================================
+  // function UserGreeting(props) {
+  //   return <h1>Welcome back!</h1>;
+  // }
+  
+  // function GuestGreeting(props) {
+  //   return <h1>Please sign up.</h1>;
+  // }
+
+  // function Greeting(props) {
+    
+  //   const isLoggedIn = props.isLoggedIn;
+  //   if (isLoggedIn) {
+  //     return <UserGreeting />;
+  //   }
+  //   return <GuestGreeting />;
+  // }
+
+  // function LoginButton(props) {
+  //   return (
+  //     <button onClick={props.onClick}>
+  //       Login
+  //     </button>
+  //   );
+  // }
+  
+  // function LogoutButton(props) {
+  //   return (
+  //     <button onClick={props.onClick}>
+  //       Logout
+  //     </button>
+  //   );
+  // }
+  // class LoginControl extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.handleLoginClick = this.handleLoginClick.bind(this);
+  //     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  //     this.state = {isLoggedIn: false};
+  //   }
+  
+  //   handleLoginClick() {
+  //     this.setState({isLoggedIn: true});
+  //   }
+  
+  //   handleLogoutClick() {
+  //     this.setState({isLoggedIn: false});
+  //   }
+  
+  //   render() {
+  //     const isLoggedIn = this.state.isLoggedIn;
+  //     let button;
+  //     if (isLoggedIn) button = <LogoutButton onClick={this.handleLogoutClick} />;
+  //     else button = <LoginButton onClick={this.handleLoginClick} />;
+  
+  //     return (
+  //       <div>
+  //         <Greeting isLoggedIn={isLoggedIn} />
+  //         {button}
+  //       </div>
+  //     );
+  //   }
+  // }
+  
+  // ReactDOM.render(
+  //   <LoginControl />,
+  //   document.getElementById('root')
+  // );
+
+  //==============================
+  function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        <h1>Hello!</h1>
+        {unreadMessages.length > 0 &&
+          <h2>
+            You have {unreadMessages.length} unread messages.
+          </h2>
+        }
       </div>
     );
-    ReactDOM.render(element, document.getElementById('root'));
   }
   
-  setInterval(tick, 1000);
+  const messages = [];
+  ReactDOM.render(
+    <Mailbox unreadMessages={messages} />,
+    document.getElementById('root')
+  );
+
+
+
+  
